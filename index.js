5eeeb633d743613eb28f053bf120fa0ec813ec47
Sergey Lypko <lypko.sg@gmail.com>
2020-10-13 15:13:23 +0300

    alpha version 0.2.0

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import WizardScene from 'telegraf/scenes/wizard/index.js';
 
+const { Extra, session, Stage, Markup, BaseScene } = telegraf;
+
+// TODO: formattin text -> red color for delete actions and so on
+
+// TODO: check if suck movie already exist in DB
+
+// TODO: in delete mode - check if record could be deleted only by it's author
+
 // TODO: have some kind of priority or willing status
 // TODO: show topbar status while loading
-
-/* - - - - - - - - - - - - - - - - - - - - */
 
 dotenv.config();
 
 /* - - - - - - - - - - - - - - - - - - - - */
 
-const noteSchema = new mongoose.Schema({
-  label: String
+export const movieSchema = new mongoose.Schema({
+  label: String,
+  id: String
 });
 
-const Note = mongoose.model('note', noteSchema);
+const Movie = mongoose.model('movie', movieSchema);
 
 const connectionParams={
   useNewUrlParser: true,
@@ -25,53 +32,55 @@ const connectionParams={
 }
 
 mongoose.connect(process.env.DB_URL, connectionParams)
-  .then(() => console.log('Connected to database'))
   .catch((err) => console.error(`Error connecting to the database. \n${err}`))
 
 /* - - - - - - - - - - - - - - - - - - - - */
 
 const botWelcomeMessage = `Hey you!`;
-const botHelpMessage = `Это еще что... если хочешь отменить действие добавления - просто напиши "Отмена" или "Cancel"`;
+const botHelpMessage = `Commands: /add, /all, /delete`;
 
 const bot = new telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply(botWelcomeMessage));
 bot.help((ctx) => ctx.reply(botHelpMessage));
 
-const addMovieWizardID = 'add_movie_wizard';
+/* - - - - - - - - - Add - - - - - - - - - - - */
 
+export const addMovieWizardID = 'add_movie_wizard';
 
-const { Extra, session, Composer, Stage, Markup, BaseScene } = telegraf;
-
-
-const addMovieWizard = new WizardScene(
+export const addMovieWizard = new WizardScene(
   addMovieWizardID,
   (ctx) => {
-    ctx.reply('Название: ');
+    ctx.reply('Name: ');
     return ctx.wizard.next();
   },
   async (ctx) => {
     const { text } = ctx.message;
 
-    if (text === "Отмена" || text === "Cancel") {
-      ctx.reply('Принято, отменяю.');
+    // TODO: add cancel button to keyboard
+
+    // TODO: config file with texts
+
+    if (text === "cancel" || text === "Cancel") {
+      ctx.reply('Canceled');
       return ctx.scene.leave();
     }
 
     if (text.length < 2) {
-      ctx.reply('Название слишком короткое, попробуй снова: ');
+      ctx.reply('The name is too short: ');
       return;
     }
 
-    const note = new Note({ label: text });
+    const movie = new Movie({ label: text });
 
-    await note.save(err => {
+    await movie.save(err => {
       if (err) {
-        ctx.reply('К сожалению не удалось сохранить новый фильм.');
+        ctx.reply('Could not save the movie record');
         return null;
       }
 
-      ctx.reply(`Позиция "${text}" была добавлена в список.`);
+      ctx.replyWithHTML(`Record <i><b>${text}</b></i> has been saved`);
+      // showMovies(ctx);
     });
 
     return ctx.scene.leave();
@@ -83,47 +92,61 @@ const stage = new Stage([addMovieWizard]);
 bot.use(session());
 bot.use(stage.middleware());
 
-
-bot.command('all', ctx => {
-  Note.find()
-    .then(notes => {
-      // TODO: formatted list with appropriate UI
-
-      notes.forEach(({ label }) => {
-        ctx.reply(label);
-      })
-    });
-});
-
 bot.command('add', ctx => ctx.scene.enter(addMovieWizardID));
 
+/* - - - - - - - - - Delete - - - - - - - - - - - */
 
-bot.command('delete', (ctx) => {
-  Note.find()
-    .then(notes => {
-      const createButtons = m => notes.map(note => [m.callbackButton(
-        note.label,
-        JSON.stringify({ a: 'delete', p: note.label }),
-        false
-      )]);
-      const itemKeyboard = Extra.HTML().markup((m) => m.inlineKeyboard(createButtons(m), {}));
+bot.command('delete', async (ctx) => {
+  try {
+    const movies = await Movie.find();
 
-      ctx.reply('Delete item: ', itemKeyboard);
-    });
+    const createButtons = m => movies.map(({ _id, label }) => [m.callbackButton(
+      label,
+      JSON.stringify({ action: 'delete', payload: _id }),
+      false
+    )]);
+
+    const itemKeyboard = Extra.HTML().markup((m) => m.inlineKeyboard(createButtons(m), {}));
+    ctx.reply('Delete item: ', itemKeyboard);
+  } catch (err) {
+    ctx.reply('Could not make an action');
+  }
 });
 
-function exposeMovie(ctx, next) {
-  const actionData = JSON.parse(ctx.callbackQuery.data);
-  ctx.movie = actionData;
+ async function deleteAction (ctx) {
+  const { payload: id } = JSON.parse(ctx.callbackQuery.data);
 
-  return next();
-}
+  const { label } = await Movie.findById(id);
 
-const deleteAction = async (ctx) => {
-  console.log(ctx.movie);
-  console.log('-----------');
+  try {
+    await Movie.findByIdAndDelete(id);
+    ctx.replyWithHTML(`Record <i><b>${label}</b></i> has been removed`);
+    // showMovies(ctx);
+  } catch {
+    ctx.reply('Could not remove record');
+  }
 };
 
-bot.action(/delete/, exposeMovie, deleteAction);
+bot.action(/delete/, deleteAction);
+
+/* - - - - - - - - - - All - - - - - - - - - - */
+
+async function showMovies(ctx) {
+  try {
+    const movies = await Movie.find();
+    ctx.reply('----------')
+    return movies.forEach(({ label }) => ctx.reply(label))
+  } catch {
+    ctx.reply('Could not make an action');
+  }
+}
+
+bot.command('all', showMovies);
+
+/* - - - - - - - - - - - - - - - - - - - - */
+
 
 bot.launch()
+
+// ctx.reply("message", { reply_markup: { remove_keyboard: true}});
+  
